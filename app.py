0309e98f873bfcdae1e26e0823a1c418d87cae22
Aadigoel <aadigoel114@gmail.com>
2022-02-13 05:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,25 @@ main = st.container()
 
 with header:
     st.title("Responsible AI")
+    st.header("Team: SaaS")
+    st.subheader("Solutions-as-a-Service")
     st.markdown("""
     Most of the world is concerned about the far-off, seemingly apocalyptic potential of AI.
     But in this process we tend to overlook the immediate problem of amplifying biases of our 
     society through our programs.
 
-    > A prime example of this would be when Microsoft's commercial algorithm
-    identified some individuals with darker skin as gorillas
-
-    > LinkedIn job adverstisements should a bias towards male users
+    Three such instances are
     """)
+    col1, col2, col3 = st.columns(3)
+
+    col1.subheader("Google's Face recognition algorithm")
+    col1.write("Classified dark skinned males as gorillas")
+
+    col2.subheader("LinkedIn's job advertisements")
+    col2.write("Showed preference to male names in searches")
+
+    col3.subheader("Microsoft's AI chatbot Tay")
+    col3.write("churned out anti-semitic sentences after being trained on Twitter for one day")
+
+    st.caption("But how can a computer program be racist?")
 
